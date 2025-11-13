44b9ffcf1cfd15008219fa3a641b1677c973d50a
Santiago Oroz <Santiago.oroz1510@gmail.com>
2025-11-13 08:56:10 -0300

    elimino script de prueba

--- aida_bot/prueba.py
+++
@@ -1,8 +0,0 @@
-import sys, os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from aida_bot.storage.database import get_storage_client
-
-
-db = get_storage_client()
-db.save_profile("test_user", {"nombre": "AIDA", "rol": "asistente"})
-print(db.get_profile("test_user"))
